6e66e02921959d2490023cfeff083439b3f2b1f0
MukhametdinovAF <ekb-led2012@yandex.ru>
2022-01-11 14:27:08 +0500

    110122

--- src/Timus/Task2066.java
+++ src/Timus/Task2066.java
@@ -13,15 +13,14 @@ public class Task2066 {
         int c = in.nextInt();
         int[] abc = new int[]{a, b, c};
         Arrays.sort(abc);
-        int count = 0;
-        if (abc[0] == abc[1]) {
-            if (abc[1] == abc[2]) {
-                count = abc[0] - abc[1] - abc[2];
-            }
+        int count;
+
+        if (abc[0] == abc[1] && abc[0] == 1||abc[0]==0) {
+           count = abc[0] - abc[1] - abc[2];
+
         } else count = abc[0] - abc[1] * abc[2];
         System.out.println(count);
-    }
-}
+    }}
 
 
 
