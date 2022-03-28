a6ca5b558561640c9f11ec2591d15fab7bee7372
MukhametdinovAF <96645172+MukhametdinovAF@users.noreply.github.com>
2022-03-28 12:47:29 +0500

    280322

--- src/Programming/laba8/hometask6/Example6.java
+++ src/Programming/laba8/hometask6/Example6.java
@@ -7,13 +7,11 @@ public class Example6 {
     public Example6 next;
 
 
-
-
     public Example6(int value, Example6 next) {
         this.value = value;
         this.next = next;
     }
-            
+
     public static Example6 createHead(int size) {
         Example6 head = new Example6(1, null);
         Example6 tail = head;
@@ -25,48 +23,48 @@ public class Example6 {
         return head;
     }
 
-    public static Example6 createTail(int size){
-              Example6 head = null;
-            for (int i = size; i >0; i--) {
-                head = new Example6(i,head);
+    public static Example6 createTail(int size) {
+        Example6 head = null;
+        for (int i = size; i > 0; i--) {
+            head = new Example6(i, head);
         }
         return head;
     }
 
-    public static void toStringMethod(String Message, Example6 head){
+    public static void toStringMethod(String Message, Example6 head) {
         Example6 ref = head;
         System.out.println(Message);
-        while(ref!=null){
-            System.out.println("value= "+ ref.value);
-            ref=ref.next;
+        while (ref != null) {
+            System.out.println("value= " + ref.value);
+            ref = ref.next;
         }
     }
 
-    public static Example6 AddFirst(Example6 head,int value) {
-        return new Example6(value,head);
+    public static Example6 AddFirst(Example6 head, int value) {
+        return new Example6(value, head);
     }
 
-    public static void AddLast(Example6 head, int value){
+    public static void AddLast(Example6 head, int value) {
         Example6 ref = head;
         while (ref.next != null) {
             ref = ref.next;
 
         }
-        ref.next=new Example6(value,null);
+        ref.next = new Example6(value, null);
     }
 
-    public static void Insert(Example6 head,int value,int position){
+    public static void Insert(Example6 head, int value, int position) {
         Example6 newposition = new Example6(value, null);
-        int index=1;
+        int index = 1;
         Example6 ref = head;
 
-            while(ref.next!=null&&index<position){
-                ref=ref.next;
-                index++;
-            }
-            newposition.next=ref.next.next;
-            ref.next=newposition;
+        while (ref.next != null && index < position) {
+            ref = ref.next;
+            index++;
         }
+        newposition.next = ref.next.next;
+        ref.next = newposition;
+    }
 
     public static Example6 RemoveFirst(Example6 head) {
         head = head.next;
@@ -81,39 +79,41 @@ public class Example6 {
         }
         ref.next = null;
     }
-    public static void Remove(Example6 head, int position){
-        int index=1;
-        Example6 ref = head;
-            while(ref.next!=null&&index<position){
-                ref=ref.next;
-                index++;
-            }
 
-            ref.next=ref.next.next;
+    public static void Remove(Example6 head, int position) {
+        int index = 1;
+        Example6 ref = head;
+        while (ref.next != null && index < position) {
+            ref = ref.next;
+            index++;
         }
 
-    public static void createHeadRec(Example6 head,Example6 tail,int value){
-        if(head!=null) {
+        ref.next = ref.next.next;
+    }
+
+    public static void createHeadRec(Example6 head, Example6 tail, int value) {
+        if (head != null) {
             tail = head;
         }
-        if(value!=1){
+        if (value != 1) {
             tail.next = new Example6(value - 1, null);
             tail = tail.next;
-            createHeadRec(null,tail,value-1);
+            createHeadRec(null, tail, value - 1);
         }
     }
 
-    public static Example6 createTailRec(Example6 head,int value){
-        if(value!=1){
+    public static Example6 createTailRec(Example6 head, int value) {
+        if (value != 0) {
             head = new Example6(value, head);
-            head = createTailRec(head,value-1);
+            head = createTailRec(head, value - 1);
         }
         return head;
     }
-    public void toStringRec(Example6 node){
+
+    public void toStringRec(Example6 node) {
         Example6 head = null;
-        if(value>=0){
-            head = new Example6(value,head);
+        if (value >= 0) {
+            head = new Example6(value, head);
             value--;
             toStringRec(node);
         }
@@ -124,97 +124,68 @@ public class Example6 {
         }
     }
 
-    public int getLength(Example6 node){
-        int length=0;
-        Example6 head = new Example6(value,null);
+    public int getLength(Example6 node) {
+        int length = 0;
+        Example6 head = new Example6(value, null);
         Example6 tail = head;
 
         for (int i = 0; i < value; i++) {
             tail.next = new Example6(i + 1, null);
             tail = tail.next;
         }
-        Example6 ref=head;
-        while(ref.next!=null){
-            ref=ref.next;
+        Example6 ref = head;
+        while (ref.next != null) {
+            ref = ref.next;
             length++;
         }
         return length;
     }
-    public void getLengthForThree(Example6 node){
-        Example6 head = new Example6(value,next);
-        Example6 tail = head;
-        for (int i=0;i<value;i++){
-            tail.next=new Example6(i+1,null);
-            tail=tail.next;
-        }
-        Example6 ref=head;
-        int counter=0;
-        int sum=0;
-        int middle=0;
-        while(ref.next!=null){
-            ref=ref.next;
-            if(ref.value%3==0){
-            counter++;
-            sum=sum+ref.value;
-            middle=sum/counter;
-            }
-        }
-        System.out.println("Количество чисел последовательности, делящихся на 3 = "+counter);
-        System.out.println("Сумма этих чисел = "+sum);
-        System.out.println("Среднее арифметическое этих чисел = "+middle);
-    }
 
-    public void getMinAndMaxForThree(Example6 node){
-        Example6 head = new Example6(value,next);
-        Example6 tail = head;
-        for(int i = 0; i<value; i++){
-            tail.next = new Example6(i+1,null);
-            tail = tail.next;
-        }
+    public static void getLengthForThree(Example6 head) {
         Example6 ref = head;
-        int min=0;
-        int max=0;
-        while (ref.next!=null){
-            ref=ref.next;
-            if (ref.value%3==0 && ref.value<4) {
-                min = ref.value;
-            }
-            else if (ref.value % 3 == 0 && ref.value > min) {
-                    max = ref.value;
-                }
-        }
-        ref=head;
-        Example6 setmax = new Example6(max,null);
-        Example6 setmin = new Example6(min,null);
-        System.out.println("Список элементов выглядит следующим образом ");
-        while (ref.next!=null){
-            ref=ref.next;
-
-            if (ref.value==min){
-
-                ref.value=setmax.value;
-                setmax.next=ref.next;
-
-            }
-            else if (ref.value==max){
-                ref.value=setmin.value;
-                setmin.next=ref.next;
-
+        int counter = 0;
+        int sum = 0;
+        int middle = 0;
+        while (ref.next != null) {
+            ref = ref.next;
+            if (ref.value % 3 == 0) {
+                counter++;
+                sum = sum + ref.value;
+                middle = sum / counter;
             }
         }
-        Example6 refout =head;
-        while (refout.next!=null){
-            refout=refout.next;
-            System.out.print(" "+refout.value);
-        }
-
-
+        System.out.println("Количество чисел последовательности, делящихся на 3 = " + counter);
+        System.out.println("Сумма этих чисел = " + sum);
+        System.out.println("Среднее арифметическое этих чисел = " + middle);
     }
 
-
-
-
-
+    public static void getMinAndMaxForThree(Example6 head) {
+        Example6 ref = head;
+        int min = 0;
+        int max = 0;
+        while (ref.next != null) {
+            ref = ref.next;
+            if (ref.value % 3 == 0 && ref.value < 4) {
+                min = ref.value;
+            } else if (ref.value % 3 == 0 && ref.value > min) {
+                max = ref.value;
+            }
+        }
+        ref = head;
+        Example6 setmax = new Example6(max, null);
+        Example6 setmin = new Example6(min, null);
+        System.out.println("Список элементов выглядит следующим образом ");
+        while (ref.next != null) {
+            ref = ref.next;
+            if (ref.value == min) {
+                ref.value = setmax.value;
+                setmax.next = ref.next;
+            } else if (ref.value == max) {
+                ref.value = setmin.value;
+                setmin.next = ref.next;
+            }
+        }
+    }
 }
 //        Разработать проект, в котором для ввода, вывода и изменения
 //        односвязного линейного списка создать следующие методы:
--- src/Programming/laba8/hometask6/Main.java
+++ src/Programming/laba8/hometask6/Main.java
@@ -10,37 +10,67 @@ public class Main {
         System.out.println("Введите необходимое количество чисел для формирования списка ");
         int value = in.nextInt();
 //      //Создание списка с головы:
+
 //        Example6 head = Example6.createHead(value);
 //        Example6.toStringMethod("CreateHead",head);
+
 //      //Создание списка с хвоста:
+
 //        Example6 head =Example6.createTail(value);
 //        Example6.toStringMethod("Create tail",head);
+
 //      //Добавление элемента в начало списка:
+
 //        Example6 newHead = Example6.AddFirst(head,150);
 //        Example6.toStringMethod("AddFirst",newHead);
+
 //      // Добавление элемента в конец списка:
+
 //        Example6.AddLast(head,123);
 //        Example6.toStringMethod("AddLast",head);
+
 //      // Вставка элемента в указанное место
+
 //        Example6.Insert(head,196,3);
 //        Example6.toStringMethod("Insert",head);
+
 //      // Удаление первого элемента списка
+
 //        Example6 newHeadAfterRemove=Example6.RemoveFirst(head);
 //        Example6.toStringMethod("RemoveFirst",newHeadAfterRemove);
+
 //      // Удаление последнего элемента списка
+
 //        Example6.RemoveLast(head);
 //        Example6.toStringMethod("RemoveLast",head);
+
 //      // Удаление заданного элемента:
+
 //        Example6.Remove(head,4);
 //        Example6.toStringMethod("Remove",head);
+
 //      // Ввод с головы помощью рекурсии:
+
 //        Example6 head=new Example6(0,null);
 //        Example6 tail = head;
 //        Example6.createHeadRec(head,tail,value);
 //        Example6.toStringMethod("CreateHeadRec",head);
+
 //      // Ввод с хвоста с помощью рекурсии:
 
-        Example6 head=Example6.createTailRec(null,value);
-        Example6.toStringMethod("CreateTailRec",head);
+//        Example6 head=Example6.createTailRec(null,value);
+//        Example6.toStringMethod("CreateTailRec",head);
+//        // Дополнительное задание:
+//        // Создание списка из n целых чисел:
+//        //
+//        //А) Найти сумму, количество и среднее значение среди чисел, делящихся на 3.
+
+//        Example6.getLengthForThree(head);
+//
+//        //Б) Найти среди чисел, которые делятся на 3, минимальное и максимальное
+//значения и их номера и поменять их местами.
+
+//        Example6.getMinAndMaxForThree(head);
+//        Example6.toStringMethod("GetLengthForThree",head);
     }
 }
