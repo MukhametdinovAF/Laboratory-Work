b7427d772213f58e7c146594bbca7da4ea7b4e14
MukhametdinovAF <96645172+MukhametdinovAF@users.noreply.github.com>
2022-04-13 17:10:53 +0500

    130422

--- src/Programming/laba10/task3/task3.java
+++ src/Programming/laba10/task3/task3.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class task3 {
 
         public static String split (String [] words){
+            int counter=0;
             String resuilt="";
             char c;
             char [] sogl ={'Б','б','В','в','Г','г','Д','д','Ж','ж','З','з','Й','й','К','к','Л','л','М','м','Н','н','П','п','Р','р','С','с','Т','т','Ф','ф','Х','х','Ц','ц','Ч','ч','Ш','ш','Щ','щ'};
@@ -12,10 +13,11 @@ public class task3 {
                 for(int j=0;j< sogl.length;j++){
                     if(c==sogl[j]){
                         resuilt=resuilt+words[i]+" ";
+                        counter++;
                     }
                 }
             }
-            return resuilt;
+            return resuilt+"выбрано слов: "+counter;
         }
 
         public static void main(String[] args) {
@@ -23,9 +25,10 @@ public class task3 {
         BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\ekb-l\\IdeaProjects\\Laboratory Work\\src\\Programming\\laba10\\task3\\text2",false)))){
             String poetry;
             int count=1;
+
             while ((poetry= br.readLine())!=null){
                 String [] words = poetry.split(" ");
-                wr.write(count+" "+task3.split(words));
+                wr.write(count+" "+(task3.split(words)));
                 wr.newLine();
                 count++;
             }
